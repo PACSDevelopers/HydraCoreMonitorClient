95aa4eada35846cf06fc4d722b362c908ff5c47d
Ryan Howell <ryan@theryanhowell.co.uk>
2014-12-02 12:23:51 +0000

    Updating

--- core/modules/system/mvc.class.php
+++ core/modules/system/mvc.class.php
@@ -112,7 +112,7 @@
                                         $status = $page->init($this->GET, $this->POST);
                                     }
 
-									if($status === 307 || $status === 302 || $status === 301) {
+                                    if($status === 301 || $status === 302 || $status === 307) {
 										$page->setRendered(true);
 										$page = null;
 										unset($page);
